fbbfede4e069805190c4c9db1f79e6d76a8e95be
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-02-01 20:36:20 -0500

    Removed a res.redirect

--- faction/api/services/passport.js
+++ faction/api/services/passport.js
@@ -196,7 +196,8 @@ passport.endpoint = function (req, res) {
   // If a provider doesn't exist for this endpoint, send the user back to the
   // login page
   if (!strategies.hasOwnProperty(provider)) {
-    return res.redirect('/login');
+    // return res.redirect('/login');
+    return res.json(error:"No provider");
   }
 
   // Attach scope if it has been set in the config
